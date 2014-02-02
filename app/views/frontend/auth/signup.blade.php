dfe1834371e2aaac0e4e324fdd648200741ce113
neovu <baovuhbv@gmail.com>
2014-02-02 14:31:50 +0700

    Sửa giao diện Trang Đăng Kí

--- app/views/frontend/auth/signup.blade.php
+++ app/views/frontend/auth/signup.blade.php
@@ -9,79 +9,81 @@ Account Sign up ::
 {{-- Page content --}}
 @section('content')
 <div id="main" class="container">
-	<div class="page-header">
-		<h3>Sign up</h3>
-	</div>
-	<div class="row">
-		<form method="post" action="{{ route('signup') }}" class="form-horizontal" autocomplete="off">
-			<!-- CSRF Token -->
-			<input type="hidden" name="_token" value="{{ csrf_token() }}" />
+	<article class="col-md-4 col-md-offset-4">
+		<div class="page-header">
+			<h3>Sign up</h3>
+		</div>
+		<div class="row">
+			<form method="post" action="{{ route('signup') }}" class="form-horizontal" autocomplete="off">
+				<!-- CSRF Token -->
+				<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-			<!-- First Name -->
-			<div class="control-group{{ $errors->first('first_name', ' error') }}">
-			<label class="control-label" for="first_name">First Name</label>
-				<div class="controls">
-					<input type="text" name="first_name" id="first_name" value="{{ Input::old('first_name') }}" />
-					{{ $errors->first('first_name', '<span class="help-block">:message</span>') }}
+				<!-- First Name -->
+				<div class="control-group{{ $errors->first('first_name', ' error') }}">
+				<label class="control-label" for="first_name">Bạn tên gì?</label>
+					<div class="controls">
+						<input type="text" name="first_name" id="first_name" value="{{ Input::old('first_name') }}" />
+						{{ $errors->first('first_name', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<!-- Last Name -->
-			<div class="control-group{{ $errors->first('last_name', ' error') }}">
-				<label class="control-label" for="last_name">Last Name</label>
-				<div class="controls">
-					<input type="text" name="last_name" id="last_name" value="{{ Input::old('last_name') }}" />
-					{{ $errors->first('last_name', '<span class="help-block">:message</span>') }}
+				<!-- Last Name -->
+				<div class="control-group{{ $errors->first('last_name', ' error') }}">
+					<label class="control-label" for="last_name">Tên Nhân Vật</label>
+					<div class="controls">
+						<input type="text" name="last_name" id="last_name" value="{{ Input::old('last_name') }}" />
+						{{ $errors->first('last_name', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<!-- Email -->
-			<div class="control-group{{ $errors->first('email', ' error') }}">
-				<label class="control-label" for="email">Email</label>
-				<div class="controls">
-					<input type="text" name="email" id="email" value="{{ Input::old('email') }}" />
-					{{ $errors->first('email', '<span class="help-block">:message</span>') }}
+				<!-- Email -->
+				<div class="control-group{{ $errors->first('email', ' error') }}">
+					<label class="control-label" for="email">Email</label>
+					<div class="controls">
+						<input type="text" name="email" id="email" value="{{ Input::old('email') }}" />
+						{{ $errors->first('email', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<!-- Email Confirm -->
-			<div class="control-group{{ $errors->first('email_confirm', ' error') }}">
-				<label class="control-label" for="email_confirm">Confirm Email</label>
-				<div class="controls">
-					<input type="text" name="email_confirm" id="email_confirm" value="{{ Input::old('email_confirm') }}" />
-					{{ $errors->first('email_confirm', '<span class="help-block">:message</span>') }}
+				<!-- Email Confirm -->
+				<div class="control-group{{ $errors->first('email_confirm', ' error') }}">
+					<label class="control-label" for="email_confirm">Nhập lại Email</label>
+					<div class="controls">
+						<input type="text" name="email_confirm" id="email_confirm" value="{{ Input::old('email_confirm') }}" />
+						{{ $errors->first('email_confirm', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<!-- Password -->
-			<div class="control-group{{ $errors->first('password', ' error') }}">
-				<label class="control-label" for="password">Password</label>
-				<div class="controls">
-					<input type="password" name="password" id="password" value="" />
-					{{ $errors->first('password', '<span class="help-block">:message</span>') }}
+				<!-- Password -->
+				<div class="control-group{{ $errors->first('password', ' error') }}">
+					<label class="control-label" for="password">Mật Khẩu</label>
+					<div class="controls">
+						<input type="password" name="password" id="password" value="" />
+						{{ $errors->first('password', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<!-- Password Confirm -->
-			<div class="control-group{{ $errors->first('password_confirm', ' error') }}">
-				<label class="control-label" for="password_confirm">Confirm Password</label>
-				<div class="controls">
-					<input type="password" name="password_confirm" id="password_confirm" value="" />
-					{{ $errors->first('password_confirm', '<span class="help-block">:message</span>') }}
+				<!-- Password Confirm -->
+				<div class="control-group{{ $errors->first('password_confirm', ' error') }}">
+					<label class="control-label" for="password_confirm">Nhập Lại Mật Khẩu</label>
+					<div class="controls">
+						<input type="password" name="password_confirm" id="password_confirm" value="" />
+						{{ $errors->first('password_confirm', '<span class="help-block">:message</span>') }}
+					</div>
 				</div>
-			</div>
 
-			<hr>
+				<hr>
 
-			<!-- Form actions -->
-			<div class="control-group">
-				<div class="controls">
-					<a class="btn" href="{{ route('home') }}">Cancel</a>
+				<!-- Form actions -->
+				<div class="control-group">
+					<div class="controls">
+						<a class="btn" href="{{ route('home') }}">Hủy</a>
 
-					<button type="submit" class="btn">Sign up</button>
+						<button type="submit" class="btn">Đăng Kí</button>
+					</div>
 				</div>
-			</div>
-		</form>
-	</div>
+			</form>
+		</div>
+	</article>
 </div>
 @stop
